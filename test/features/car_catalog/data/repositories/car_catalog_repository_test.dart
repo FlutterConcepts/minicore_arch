1668ec4f499da65ab0bddba966d3ab959b7a936f
Kevin Kobori <kevinyujikobori@gmail.com>
2024-11-20 12:41:33 -0300

    Remove obsolete fetchCarBrands test and consolidate car catalog repository tests for improved organization and clarity

--- test/features/car_catalog/data/repositories/car_catalog_repository_test.dart
+++ test/features/car_catalog/data/repositories/car_catalog_repository_test.dart
@@ -10,15 +10,84 @@ class MockClient extends Mock implements Client {}
 
 void main() {
   late Client mockClient;
-  late CarCatalogRepository sutRepository;
+  late CarCatalogRepository sut;
 
   setUp(() {
     mockClient = MockClient();
-    sutRepository = ParallelumCarCatalogRepository(mockClient);
+    sut = ParallelumCarCatalogRepository(mockClient);
   });
 
   setUpAll(() {
     registerFallbackValue(Uri.parse(''));
+  });
+
+  group('fetchCarBrands Tests |', () {
+    test(
+        '''Should return CarBrandsSuccess when API call succeeds with valid data''',
+        () async {
+      // Arrange
+      final mockResponseData = jsonEncode(
+        List.generate(5, (_) {
+          return {
+            'code': faker.randomGenerator.integer(1000, min: 1).toString(),
+            'name': faker.vehicle.model(),
+          };
+        }),
+      );
+      when(() => mockClient.get(any())).thenAnswer(
+        (_) async => Response(mockResponseData, 200),
+      );
+
+      // Act
+      final result = await sut.fetchCarBrands();
+
+      // Assert
+      expect(result, isA<CarBrandsSuccess>());
+      final successState = result as CarBrandsSuccess;
+      expect(successState.carBrands.length, 5);
+      expect(successState.carBrands[0], isA<CarBrandEntity>());
+    });
+
+    test(
+        '''Should return CarCatalogFailure when API call fails with a non-200 status code''',
+        () async {
+      // Arrange
+      const mockStatusCode = 404;
+      final mockMessage = faker.lorem.sentence();
+      when(() => mockClient.get(any())).thenAnswer(
+        (_) async => Response(mockMessage, mockStatusCode),
+      );
+
+      // Act
+      final result = await sut.fetchCarBrands();
+
+      // Assert
+      expect(result, isA<CarCatalogFailure>());
+      final failureState = result as CarCatalogFailure;
+      expect(
+        failureState.message,
+        'Failed to fetch car brands catalog. Status code: $mockStatusCode',
+      );
+    });
+
+    test('should return CarCatalogFailure when an exception is thrown',
+        () async {
+      // Arrange
+      final mockExceptionMessage = faker.lorem.sentence();
+      when(() => mockClient.get(any()))
+          .thenThrow(Exception(mockExceptionMessage));
+
+      // Act
+      final result = await sut.fetchCarBrands();
+
+      // Assert
+      expect(result, isA<CarCatalogFailure>());
+      final failureState = result as CarCatalogFailure;
+      expect(
+        failureState.message,
+        'Failed to fetch car brands catalog: Exception: $mockExceptionMessage',
+      );
+    });
   });
 
   group('fetchCarModelsByBrand Tests |', () {
@@ -46,7 +115,7 @@ void main() {
       );
 
       // Act
-      final result = await sutRepository.fetchCarModelsByBrand(brandId);
+      final result = await sut.fetchCarModelsByBrand(brandId);
 
       // Assert
       expect(result, isA<CarModelsByBrandSuccess>());
@@ -73,7 +142,7 @@ void main() {
       );
 
       // Act
-      final result = await sutRepository.fetchCarModelsByBrand(brandId);
+      final result = await sut.fetchCarModelsByBrand(brandId);
 
       // Assert
       expect(result, isA<CarCatalogFailure>());
@@ -98,7 +167,7 @@ void main() {
       ).thenThrow(Exception(mockExceptionMessage));
 
       // Act
-      final result = await sutRepository.fetchCarModelsByBrand(brandId);
+      final result = await sut.fetchCarModelsByBrand(brandId);
 
       // Assert
       expect(result, isA<CarCatalogFailure>());
